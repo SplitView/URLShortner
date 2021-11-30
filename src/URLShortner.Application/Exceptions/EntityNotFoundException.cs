fa20435dc16df2df15dcd9cd5c6ecffcd36ffa8d
Sandesh Karki <metimelord55@outlook.com>
2021-11-30 17:39:46 +1100

    removed unnecessary projects

--- src/URLShortner.Application/Exceptions/EntityNotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace URLShortner.Application.Exceptions
-{
-    public class EntityNotFoundException : Exception
-    {
-        public EntityNotFoundException(string message) : base(message) { }
-    }
-}
